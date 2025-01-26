9ac6acedd5c4ee994ab3050b9a84c374e3758515
Mostafa Ramadan <148629370+Eng-MostafaRamadan@users.noreply.github.com>
2025-01-26 12:59:37 +0200

    type the logic

--- Day1_Task/task2.dart
+++ Day1_Task/task2.dart
@@ -1,1 +1,12 @@
-void main() {}
+void main() {
+  checkNumber(number: 10);
+}
+
+int checkNumber({required int number}) {
+  if (number % 2 == 0) {
+    print("the number is even");
+  } else {
+    print("the number is odd");
+  }
+  return 0;
+}
